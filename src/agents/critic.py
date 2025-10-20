a96949491b4228127338f294531676951b69c877
vanminhng <minhnv14099@gmail.com>
2025-10-20 08:17:22 -0400

    feat (src/agents/critic.py): add critic agent
    - receive a script generated by coding agent
    - analyze and provide fixes for critics existing on rendered images
    
    Signed-off-by: vanminhng <minhnv14099@gmail.com>

--- src/agents/critic.py
+++ src/agents/critic.py
@@ -2,16 +2,152 @@
 #  Copyright (c) 2025
 #  Minh NGUYEN <vnguyen9@lakeheadu.ca>
 #
+import logging
+import os
+from pathlib import Path
+
+from langchain_core.language_models import BaseChatModel
+from langgraph.config import RunnableConfig
 from typing_extensions import override
 
-from src.base.agent import AgentAsNode
+from src import OutputT
+from src.base.agent import AgentAsNode, register
+from src.base.typing import InputT
+from src.base.utils import DirectionRouter, Command, Send
+from src.utils.script import (
+    write_script,
+    execute,
+    CAMERA_SETTING_FILE
+)
+
+logger = logging.getLogger(__name__)
 
 
+@register(type="agent", name='critic')
 class CriticAgent(AgentAsNode, name='Critic'):
     """
     The Critic Agent class
     """
 
+    def __init__(
+            self,
+            metadata: dict = None,
+            input_schema: InputT | dict = None,
+            edges: dict[str, tuple[str]] = None,
+            tool_schemas: list | list[dict] = None,
+            output_schema: OutputT | list[OutputT] | list[dict] = None,
+            model_name: str = None,
+            model_provider: str = None,
+            model_api_key: str = None,
+            output_schema_as_tool: bool = None,
+            chat_model: BaseChatModel = None,
+            save_rendered_dir: str = None,
+            anchor_script_path: str = None,
+            validating_prompt: str = None,
+            **kwargs
+    ):
+        super().__init__(
+            metadata,
+            input_schema,
+            edges,
+            tool_schemas,
+            output_schema,
+            model_name,
+            model_provider,
+            model_api_key,
+            output_schema_as_tool,
+            chat_model,
+            **kwargs
+        )
+
+        self.camera_setting_file = kwargs.get("camera_setting_file", CAMERA_SETTING_FILE)
+        self.combined_script_template = """{creation_script}
+        
+{additional_script} 
+"""
+        self.save_rendered_dir = save_rendered_dir
+        os.makedirs(save_rendered_dir, exist_ok=True)
+
+        self.anchor_script_path = anchor_script_path
+        os.makedirs(Path(anchor_script_path).parent, exist_ok=True)
+
+        self.validating_prompt = validating_prompt
+
     @override
-    def __call__(self, state, *args, **kwargs):
+    def __call__(
+            self,
+            state: InputT | dict,
+            runtime: RunnableConfig = None,
+            context: RunnableConfig = None,
+            config: RunnableConfig = None,
+            **kwargs
+    ) -> Command | Send | dict:
+        script = state['current_script']
+        processed_script = self._process_script(script)
+
+        rendered_image_paths = self._run_to_get_rendered_images(processed_script)
+        predefined_prompt = self.validating_prompt
+
+        critic_fixes = []
+        for p in rendered_image_paths:
+            critic_fixes.extend(self.anchor_call(
+                image_path=p,
+                prompt=predefined_prompt)
+            )
+
+        # ----------process critic fixes-----------
+        #
+        # -----------------------------------------
+
+        update_state = {
+            'queries': [d['fixes'] for d in critic_fixes],
+            'coding_task': 'improve',
+            'is_sub_call': False,
+            'caller': 'critic',
+            'has_docs': False
+        }
+
+        return DirectionRouter.go_next(
+            method='command',
+            state=update_state,
+            node='coding'
+        )
+
+    def check_critic_fixes(self, critic_fixes: list[dict]):
         ...
+
+    def _run_to_get_rendered_images(self, script: str):
+        write_script(script, self.anchor_script_path)
+        result = execute(script_path=self.anchor_script_path)
+
+        rendered_image_paths = os.listdir(self.save_rendered_dir)
+        return rendered_image_paths
+
+    def _process_script(self, script):
+        with open(self.camera_setting_file, mode='r') as f:
+            camera_script = f.read()
+        camera_script = camera_script.replace("{save_dir}", self.save_rendered_dir)
+
+        _script = self.combined_script_template.format(
+            creation_script=script,
+            additional_script=camera_script,
+        )
+
+        return _script
+
+    @override
+    def anchor_call(self, image_path: str, prompt: str):
+        return [
+            {
+                'critic': 'The armrests aren\'t attached to the seat',
+                'fixes': 'Move armrests down by y-axis'
+            },
+            {
+                'critic': 'The legs are too long',
+                'fixes': 'Decrease length of legs'
+            }
+        ]
+
+    @override
+    def chat_model_call(self, image_paths: list[str], prompt: str, *args, **kwargs):
+        raise NotImplementedError
