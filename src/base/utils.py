5e715e505681cd0617016701890c94814dfd1289
vanminhng <minhnv14099@gmail.com>
2025-10-20 08:19:36 -0400

    feat (src): add some utilized functions
    Signed-off-by: vanminhng <minhnv14099@gmail.com>

---
+++ src/base/utils.py
@@ -0,0 +1,23 @@
+#
+#  Copyright (c) 2025
+#  Minh NGUYEN <vnguyen9@lakeheadu.ca>
+#
+from typing import Union, Literal
+
+from langgraph.types import Command, Send
+
+
+class DirectionRouter:
+
+    @classmethod
+    def go_next(
+            cls,
+            state: dict,
+            node: str,
+            method: Literal['command', 'send'] = 'command'
+    ) -> Union[Command, Send, None]:
+        if method.lower() == 'command':
+            return Command(update=state, goto=node)
+        elif method.lower() == 'send':
+            return Send(arg=state, node=node)
+        return None
---
+++ src/utils/script.py
@@ -0,0 +1,64 @@
+#
+#  Copyright (c) 2025
+#  Minh NGUYEN <vnguyen9@lakeheadu.ca>
+#
+import subprocess
+
+CAMERA_SETTING_FILE = 'templates/camera_setting'
+ANCHOR_FILE = "assets/blender_script/anchor.py"
+
+
+def combine_and_write(script_path: str, additional_path: str = None):
+    with open(script_path, mode='r') as f:
+        creation_script = f.read()
+
+    if additional_path is None:
+        additional_path = CAMERA_SETTING_FILE
+
+    with open(additional_path, mode='r') as f:
+        camera_script = f.read()
+
+    script = f"""{creation_script}
+    
+{camera_script}
+"""
+
+    with open(ANCHOR_FILE, 'w') as f:
+        f.write(script)
+
+    return ANCHOR_FILE
+
+
+def execute(script_path: str):
+    python_option = '--python'
+
+    process = subprocess.Popen(
+        args=['blender', '--background', python_option, script_path],
+        shell=False,
+        restore_signals=True,
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True
+    )
+    stdout, stderr = process.communicate()
+    result = {"error": stderr, 'stdout': stdout, 'returncode': process.returncode}
+
+    process.terminate()
+    process.kill()
+    process.wait()
+
+    return result
+
+
+def write_script(script: str, file_path: str) -> None:
+    """Write a python script to a file with file path
+
+    Args:
+        script (str): Script need to write
+        file_path (str|Path): Destination will contain the script
+
+    Returns: None
+    """
+    with open(file_path, mode='w') as f:
+        f.write(script)
