d6ae7a3496518456cb7a92b90ac76cc633d8e4d5
vanminhng <minhnv14099@gmail.com>
2025-10-26 00:20:28 -0400

    feat (src/task/prepare_db.py): build vector database
    Signed-off-by: vanminhng <minhnv14099@gmail.com>

--- src/task/prepare_db.py
+++ src/task/prepare_db.py
@@ -2,11 +2,13 @@
 #  Copyright (c) 2025
 #  Minh NGUYEN <vnguyen9@lakeheadu.ca>
 #
+import glob
 import logging
 
 import faiss
+import tqdm
 from langchain_community.docstore import InMemoryDocstore
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -14,33 +16,24 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-data_dir = 'data/raw'
-db_save_dir = 'vectorstores/faiss'
+data_dir = 'data/interm/blender_python_reference_4_5/'
+raw = 'data/raw'
+db_save_dir = 'vectorstores/faiss_full'
 
 
 def build_vectorstore():
-    # loader = PyPDFLoader("data/interm/blender_python_reference_4_5/bpy_extras.io_utils.pdf")
-    dir_loader = PyPDFDirectoryLoader(
-        path=data_dir,
-        glob=r"**/*.pdf",
-        mode="single",
-        recursive=False,
-        silent_errors=False,
-        password=None,
-        extract_images=False,
-        load_hidden=False,
-    )
-
-    logger.info("Load directory pdfs")
-    docs = dir_loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=100,
-    )
-
-    logger.info("Split docs")
-    chunks = text_splitter.split_documents(docs)
+    # dir_loader = PyPDFDirectoryLoader(
+    #     path=data_dir,
+    #     # glob=r"**/*.pdf",
+    #     recursive=False,
+    #     silent_errors=False,
+    #     password=None,
+    #     extract_images=False,
+    #     load_hidden=False,
+    # )
+    #
+    # logger.info("Load directory pdfs")
+    # docs = dir_loader.load()
 
     logger.info("Initialize embedding model")
     embedding_model = GPT4AllEmbeddings()
@@ -57,8 +50,24 @@ def build_vectorstore():
         index_to_docstore_id={},
     )
 
-    logger.info('Add chunks (documents) to vectorstore')
-    vector_store.add_documents(documents=chunks)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100,
+    )
+
+    pdf_files = glob.glob(fr'{data_dir}/*.pdf')
+
+    batch_size = 20
+    for i in tqdm.tqdm(range(0, len(pdf_files), batch_size)):
+        batch_files = pdf_files[i:i + batch_size]
+        batch_docs = []
+        for file in tqdm.tqdm(batch_files):
+            loader = PyPDFLoader(file)
+            docs = loader.lazy_load()
+            batch_docs.extend(docs)
+
+        chunks = text_splitter.split_documents(batch_docs)
+        vector_store.add_documents(chunks)
 
     logger.info(f"Save vectorstore to {db_save_dir}")
     vector_store.save_local(folder_path=db_save_dir)
