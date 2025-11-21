ace8d17927b3e1856d053228c2ca4f593c1ae842
vanminhng <minhnv14099@gmail.com>
2025-11-21 07:39:20 -0500

    update (src/agent/coding.py): keep comments on previous
    Signed-off-by: vanminhng <minhnv14099@gmail.com>

--- src/agent/coding.py
+++ src/agent/coding.py
@@ -2,10 +2,12 @@
 #  Copyright (c) 2025
 #  Minh NGUYEN <vnguyen9@lakeheadu.ca>
 #
+from __future__ import annotations
+
 import os
 import logging
 from copy import deepcopy
-from typing import Union, Generic
+from typing import Union, Generic, Optional, cast, TYPE_CHECKING, Literal
 from typing_extensions import override, overload
 
 from langchain_core.prompts import (
@@ -18,14 +20,19 @@ from langgraph.runtime import Runtime
 from langgraph.graph.state import END
 from langgraph.types import Command, Send
 
+from src.node import BaseNode
 from src.registry import RegisterAgent, RegisterNode
-from src.types import InputT, OutputT
-from src.base.node import AgentAsNode
-from src.base.tool import execute_script, write_script
-from src.base.utils import DirectionRouter
+from src.types import StateT, ContextT, InputT, OutputT
+from src.state.comp_5112 import CodingState
+from src.tool.func import execute_script, write_script
+from src.utils import DirectionRouter
 from src.utils.decorator import add_note_docstring
 from src.utils.exception import ScriptWithError, ExceedFixErrorAttempts
 from src.utils.file import load_prompt_template_file
+
+if TYPE_CHECKING:
+    from langchain_core.prompt_values import PromptValue
+    from src.message.parsed_tool_call import ParsedTollCallMessage
 
 logger = logging.getLogger(__name__)
 
@@ -33,7 +40,12 @@ logger = logging.getLogger(__name__)
 @add_note_docstring(docs="Used for only 'COMP-5112' project")
 @RegisterAgent(module_path=__name__, name='coding')
 @RegisterNode(module_path=__name__, name='coding')
-class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
+class CodingAgent(
+    BaseNode,
+    Generic[StateT, ContextT, InputT, OutputT],
+    node_name='Coding',
+    use_model=True
+):
     """The Coding Agent class"""
 
     @override
@@ -66,67 +78,42 @@ class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
     @override
     def __call__(
         self,
-        state: InputT | dict,
-        runtime: Runtime = None,
-        config: RunnableConfig = None,
+        state: StateT,
+        config: Optional[RunnableConfig] = None,
+        *,
+        runtime: Optional[Runtime[ContextT]] = None,
         **kwargs
-    ) -> Union[dict, Command, Send, OutputT]:
+    ) -> Command[Literal['retriever']]:
         """"""
-
         logger.info(self.opening_symbols)
-        logger.info(f"Number of messages: {len(state['messages'])}")
+        # logger.info(f"Number of messages: {len(state['messages'])}")
 
         # -------------------------------------------------------------------
         # This block is always executed only one time
         # store state from the official call (either to 'improve' or to 'generate')
-        if not state['is_sub_call']:
+        if not self.copy_state:
             logger.info(f"Copy state call from '{state['caller']}'")
-            logger.info(f'Number of queries: {len(state["queries"])}')
+            logger.info(f'Number of subtasks: {len(state["agent_response"])}')
             self.copy_state = deepcopy(state)
-            self.copy_state.pop('is_sub_call', None)
-            self.copy_state.pop('has_docs', None)
-            self.copy_state['num_queries'] = len(state['queries'])
+            self.copy_state['num_queries'] = len(state['agent_response'])
             self.copy_state['query_offset'] = 0
             self.copy_state['previous_scripts'] = []
             self.get_retrieved_docs = False
+            # self.copy_state.pop('has_docs', None)
         else:
             # 'fix' error task only can be called as inner call from 'improve' or 'generate' tasks
             pass
 
-        if not state['has_docs']:
-            logger.info("Call Retriever to get relevant documents")
-            return DirectionRouter.goto(
-                state={'coding_task': state['coding_task'],
-                       'queries': state['queries']},
-                node='retriever', method='send'
-            )
-
-        # store retrieved docs of first queries (official call)
-        if not self.get_retrieved_docs:
-            logger.info('Store retrieved docs')
-            self.copy_state['retrieved_docs'] = state['retrieved_docs']
-            self.get_retrieved_docs = True
-        # inner call this agent must pass above blocks
-        # -------------------------------------------------------------------
         # operate on each query
         try:
-            # while generating and executing a script, the error message could be raised
-            if state['coding_task'] == 'generate':
-                formatted_prompt = self._prepare_generate_prompt(state)
-            elif state['coding_task'] == "improve":
-                assert 'current_script' in state
-                formatted_prompt = self._prepare_improve_prompt(state)
-            else:
-                assert 'current_script' in state
-                formatted_prompt = self._prepare_fix_prompt(state)
-
+            formatted_prompt = self._prepare_prompt(state)
+            # TODO:
             script, messages = self._generate(formatted_prompt)
 
             # ------------error-free--------------------
-            # the generated script is error-free,
-            # it is also an ending point for recursive calls
-            self.copy_state['previous_scripts'].append(script)
+            # the generated script is error-free, an ending point of recursive calls
             self.copy_state['current_script'] = script
+            self.copy_state['previous_scripts'].append(script)
             self.copy_state['query_offset'] += 1
             self.copy_state['messages'] = messages
 
@@ -144,10 +131,12 @@ class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
             Before fix code, call Retriever Agent to get relevant documents
             `e.command` is a command call retriever with command and command script.
             """
+            self._finish_session(_logger=logger)
             return e.command
 
         # reset fix error tries after each query
         self.fix_error_tries = 0
+        updates = self.copy_state
 
         # continue with the next query and send it to coding
         if self.copy_state['query_offset'] < self.copy_state['num_queries']:
@@ -166,28 +155,34 @@ class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
                 self._save_all_scripts()
 
             # identify the next node based on the caller (i.e. previous node), only base on original call.
-            if self.copy_state['caller'] == 'planner':
-                next_node = 'critic'
-            elif self.copy_state['caller'] == 'critic':
-                next_node = 'verification'
-            elif self.copy_state['caller'] == 'verification':
-                next_node = 'verification'
-            elif self.copy_state['caller'] == 'user':
-                next_node = 'verification'
-            else:
-                next_node = END
+            # TODO:
+            # if self.copy_state['caller'] == 'planner':
+            #     next_node = 'critic'
+            # elif self.copy_state['caller'] == 'critic':
+            #     next_node = 'verification'
+            # elif self.copy_state['caller'] == 'verification':
+            #     next_node = 'verification'
+            # elif self.copy_state['caller'] == 'user':
+            #     next_node = 'verification'
+            # else:
+            next_node = END
 
+            updates['agent_response'] = updates['current_script']
+            self.copy_state = dict()
             logger.info("✅ ✅ ✅ ✅ ✅ ✅ ✅ ✅ ✅ ✅ Finish a call, move to next node ✅ ✅ ✅ ✅ ✅ ✅ ✅ ✅ ✅ ✅")
 
-        logger.info(self._used_token_prep())
         logger.info(f'coding -> {next_node}')
-        logger.info(self.ending_symbols)
+        self._finish_session(_logger=logger)
 
         # return stored original state ('copy_state' instead of 'state')
         # as there may be some sub calls from `retriever` that may change state.
-        return DirectionRouter.goto(state=self.copy_state, node=next_node, method='command')
+        return DirectionRouter.jump(
+            updates=updates,
+            jump_to=next_node,
+            method='command'
+        )
 
-    def _prepare_prompt(self, state):
+    def _prepare_prompt(self, state: Union[dict, StateT]) -> PromptValue:
         """Prepare prompt template base on task
 
         Args:
@@ -205,8 +200,8 @@ class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
         return formatted_prompt
 
     @override
-    def _prepare_chat_template(self, system_template=None, human_template=None) -> ChatPromptTemplate | None:
-        if not (system_template or human_template):
+    def _prepare_chat_template(self, system_template=None, human_template=None) -> Union[ChatPromptTemplate, None]:
+        if system_template is None and human_template is None:
             return None
 
         return super()._prepare_chat_template(
@@ -240,19 +235,19 @@ class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
 
         # that's called only when coding_task is 'generate
         # when queries, from both of 'state' and 'copy_state', are subtasks
-        query = state['queries'][self.copy_state['query_offset']]
-        docs = state['retrieved_docs'][self.copy_state['query_offset']]
+        query_and_instruction = state['agent_response'][self.copy_state['query_offset']]
+        query = query_and_instruction['query']
+        instruction = query_and_instruction['instruction']
 
         logger.info(
             f"{state['coding_task']}: query {1 + self.copy_state['query_offset']}/{self.copy_state['num_queries']}: {query}")
         logger.info(f"Number of previous scripts: {len(self.copy_state['previous_scripts'])}")
         # ---------------------------------------------------
-        formatted_prompt = self._get_pretty_formatted_prompt(
-            chat_prompt_template=chat_template,
+        formatted_prompt = chat_template.invoke(
             input={
-                "subtask": query,
+                "task": query,
                 "previous_scripts": self._dump_scripts(self.copy_state['previous_scripts']),
-                "summary": docs
+                "instruction": instruction
             }
         )
         # ---------------------------------------------------
@@ -262,14 +257,17 @@ class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
         chat_template = self._prepare_chat_template(human_template=self.human_fix_template)
 
         logger.info(f"{state['coding_task']}: {self.fix_error_tries}(tries)/{self.fix_error_attempts}(attempts)")
-        logger.info(f"error: {state['queries'][0]}")
+        # logger.info(f"error: {state['agent_response'][0]['query']}")
+
+        query_and_instruction = state['agent_response'][0]
+        error = query_and_instruction['query']
+        instruction = query_and_instruction['instruction']
         # ---------------------------------------------------
-        formatted_prompt = self._get_pretty_formatted_prompt(
-            chat_prompt_template=chat_template,
+        formatted_prompt = chat_template.invoke(
             input={
                 'current_script': state['current_script'],
-                'error': state['queries'],
-                'summary': state['retrieved_docs'][0]
+                'error': error,
+                'instruction': instruction
             }
         )
         # ---------------------------------------------------
@@ -278,19 +276,20 @@ class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
     def _prepare_improve_prompt(self, state):
         chat_template = self._prepare_chat_template(human_template=self.human_improve_template)
 
-        query = state['queries'][self.copy_state['query_offset']]
-        docs = state['retrieved_docs'][self.copy_state['query_offset']]
+        # query_and_instruction = self.copy_state['retrieved_docs'][self.copy_state['query_offset']]
+        # query = query_and_instruction['query']
+        # instruction = query_and_instruction['instruction']
+        solution = state['agent_response'][self.copy_state['query_offset']]
 
         logger.info(
             f"{state['coding_task']}: solution {1 + self.copy_state['query_offset']}/{self.copy_state['num_queries']}")
-        logger.info(f"solution: {query}")
+        logger.info(f"solution: {solution}")
         # ---------------------------------------------------
-        formatted_prompt = self._get_pretty_formatted_prompt(
-            chat_prompt_template=chat_template,
+        formatted_prompt = chat_template.invoke(
             input={
                 'current_script': state['current_script'],
-                'solution': query,
-                'summary': docs
+                'solution': solution,
+                # 'summary': instruction
             }
         )
         # ---------------------------------------------------
@@ -299,37 +298,42 @@ class CodingAgent(AgentAsNode, node_name='Coding', use_model=True):
     def _generate(self, formatted_prompt):
         while True:
             # ---------------------------Actual generation------------------------------
-            generated_script, messages = self.chat_model_call(formatted_prompt)
-            # --------------------------------------------------------------------------
-
+            response = cast(
+                "ParsedTollCallMessage",
+                self.invoke(
+                    input=formatted_prompt,
+                    config=self.config
+                )
+            )
             # call tool to write script
+            script = response.get_field('script', '')
+            script = script.encode().decode("unicode_escape")
             write_script.invoke({
-                "script": generated_script,
+                "script": script,
                 "file_path": self.check_error_file
             })
 
             # call tool to execute script
-            error = execute_script.invoke({'script': self.check_error_file})
-            tool_message = self.create_tool_message(content=error, _id='call_execute_script')
-            messages.append(tool_message)
+            stdout = execute_script.invoke(input={'script': self.check_error_file})
+            messages = self.get_messages(self.config)
+            messages.append(response)
 
             """Log conversation"""
-            self.log_conversation(logger, messages)
-            logger.info(self._used_token_prep())
+            # self.log_conversation(logger, messages)
 
             # no error yielded
-            if 'no error' in error.lower():
-                return generated_script, messages
+            if 'no error' in stdout.lower():
+                return script, messages
             else:
                 # raise the call to 'retriever' agent to fix error
-                raise ScriptWithError(command=DirectionRouter.goto(
-                    state={
-                        'current_script': generated_script,
+                raise ScriptWithError(command=DirectionRouter.jump(
+                    updates={
+                        'current_script': script,
                         'coding_task': 'fix',
-                        'queries': [error, ],
+                        'agent_response': [stdout, ],
                         'messages': messages
                     },
-                    node='retriever', method='command'
+                    jump_to='retriever', method='command'
                 ))
 
     def _dump_scripts(self, scripts):
