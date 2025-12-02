183c69911d4ed10d3552167b6ba1f3ccb4d6a19c
vanminhng <minhnv14099@gmail.com>
2025-12-02 09:50:12 -0500

    feat (src/builder.py): add building plain llm
    Signed-off-by: vanminhng <minhnv14099@gmail.com>

--- src/builder.py
+++ src/builder.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING, Union, Literal
 from typing_extensions import TypedDict, TypeAlias
-
+from collections import defaultdict
 from src.graph.base import BaseGraph
 from src.registry import load_class
 
@@ -18,26 +18,7 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
-
-class BuildingConfig(TypedDict):
-
-    name: Literal[
-        'base_chat',
-        'tool_call_generate_chat',
-        'tool_call_execute_chat',
-        'stateful_chat',
-        'tool_call_execute_stateful_chat',
-        'tool_call_generate_stateful_chat',
-        'base_agent',
-        'react_agent'
-    ]
-
-    model_name: str
-
-    template_file: str
-
-
-Available: TypeAlias = Literal[
+Available_LLM_interfaces: TypeAlias = Literal[
     'base_chat',
     'tool_call_generate_chat',
     'tool_call_execute_chat',
@@ -66,12 +47,16 @@ class Builder:
     @classmethod
     def build(
         cls,
-        type: Literal['chat', 'agent', 'graph'],
-        config: dict,
-        name: Available = None,
+        type: Literal['chat', 'agent', 'graph', 'llm'],
+        name: Available_LLM_interfaces = None,
         **kwargs
     ):
         """No need to define name in ``config``. Pass by ``name``"""
+        if type == 'llm':
+            type = 'chat'
+            name = 'base_chat'
+
+        config = kwargs if kwargs else defaultdict(lambda: None)
         config['name'] = name if name else ...
 
         if type == 'chat':
