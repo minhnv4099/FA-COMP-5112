f20108814cb4f25100353d2a56d67a62c115cb3d
vanminhng <minhnv14099@gmail.com>
2025-12-04 23:32:47 -0500

    feat (src/mcp/blender/): add blender mcp server
    Signed-off-by: vanminhng <minhnv14099@gmail.com>

---
+++ blender_mcp_entrypoint.py
@@ -0,0 +1,14 @@
+#
+#  Copyright (c) 2025
+#  Minh NGUYEN <vnguyen9@lakeheadu.ca>
+#
+from src.mcp.blender.server import main as server_main
+
+
+def main():
+    """Entry point for the blender-mcp package"""
+    server_main()
+
+
+if __name__ == "__main__":
+    main()
