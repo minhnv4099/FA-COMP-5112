784d7b8986d80df960b21b5d4c6758e2f49d9e21
vanminhng <minhnv14099@gmail.com>
2025-12-07 00:31:50 -0500

    feat (src/mcp/): add multiserver client, protocol, mixin
    - can work with prompt and resource
    
    Signed-off-by: vanminhng <minhnv14099@gmail.com>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #  Minh NGUYEN <vnguyen9@lakeheadu.ca>
 #
 import logging
+
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s][%(levelname)s][%(name)s][%(funcName)s] - %(message)s #%(lineno)d'
@@ -10,7 +11,7 @@ logging.basicConfig(
 
 from src.builder import Builder
 from src.utils import find_load_env
-from src.mcp.client import MCPClientToolExecutor
+from src.mcp.client import MultiServerMCPClient, SingleServerMCPClient
 
 find_load_env()
 
@@ -18,25 +19,32 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    llm_engine = Builder.build(type='llm', model_name="openai/gpt-5-nano")
-    mcp_client = MCPClientToolExecutor(server_script_path="src/mcp/server/file.py")
+    model_name = "amazon/nova-2-lite-v1:free"
+    llm_engine = Builder.build(type='llm', model_name=model_name)
+    mcp_client = MultiServerMCPClient(
+        server_script_path=[
+            ("blender", "blender_mcp_entrypoint.py"),
+            ("gmail", "src/mcp/server/gmail_api.py"),
+            ("filesystem", "src/mcp/server/file.py"),
+            ("weather", "src/mcp/server/weather.py"),]
+    )
 
     chat = Builder.build(
         type='agent',
-        interface='react_stateful_agent',
+        interface='react_agent',
         llm_engine=llm_engine,
         mcp_client=mcp_client,
-        tool_schemas=[
-            {
-                "type": "tool",
-                "name": "url_reader"
-            }
-        ])
+        schemas=[
+            {'type': 'tool', 'name': 'url_reader'},
+            {'type': 'chat_output', 'name': 'base'},
+        ]
+    )
 
-    response = chat.invoke('Hello, My name is Minh. what file .env says')
+    response = chat.invoke('Summarize the latest mesage I sent')
     response.pretty_print()
-    response = chat.invoke('Summarize what is my name')
-    response.pretty_print()
+    # response = chat.invoke('Summarize what is my name')
+    # response.pretty_print()
+    chat.print_conversation()
 
 
 if __name__ == '__main__':
